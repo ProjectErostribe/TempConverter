c0b94d485fa4a87ddeda77be12cf48e5d97f514a
Paidamoyo-Janet "Myles" Azehko <49720658+erostribe@users.noreply.github.com>
2019-10-08 02:16:41 -0400

    Add files via upload
    project complete

--- temperature converter/app.js
+++ temperature converter/app.js
@@ -9,11 +9,39 @@ const kelvinInput = document.querySelector('#kelvin > input');
   //  console.log(ktemp);
 //});
 
-celciusInput.addEventListener('input', function() {
-    const cTemp = parseFloat(celciusInput.value);
-    const fTemp = (cTemp * (9/5)) + 32; 
-    const kTemp = cTemp + 273.15;
-    fahrenheitInput.value = fTemp;
-    kelvinInput.value = kTemp;
-});
+function celciusToFahrenheitAndKelvin() {
+  const cTemp = parseFloat(celciusInput.value);
+  const fTemp = (cTemp * (9/5)) + 32; 
+  const kTemp = cTemp + 273.15;
+  fahrenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
+}
 
+function roundNum(num) {
+  return Math.round(num*100)/100;
+}
+
+function fahrenheitToCelciusAndKelvin() {
+  const fTemp = parseFloat(fahrenheitInput.value);
+  const cTemp = (fTemp - 32) * (5/9);
+  const kTemp = (fTemp + 459.67) * 5/9;
+  celciusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
+}
+
+function kelvinToCelciusAndFahrenheit() {
+  const kTemp = parseFloat(kelvinInput.value);
+  const cTemp = kTemp - 273.15;
+  const fTemp = 9/5 * (kTemp - 273) + 32;
+  celciusInput.value = roundNum(cTemp);
+  fahrenheitInput.value = roundNum(fTemp);
+}
+
+function main() {
+  celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
+  fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
+  kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
+
+}
+
+main();
